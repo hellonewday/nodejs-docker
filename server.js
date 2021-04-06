e561248c0185f64ab84ec75eaa242dd7a5948804
root <root@hungnq.dev>
2021-04-06 13:17:16 +0700

    change docker-compose

--- server.js
+++ server.js
@@ -6,32 +6,20 @@ const os = require("os");
 const books = require("./api/books");
 require("dotenv").config();
 
-mongoose.connect(
-  os.platform() === "win32"
-    ? `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0-4veva.gcp.mongodb.net/chatops?retryWrites=true&w=majority`
-    : `mongodb://localhost:27017/books`,
-  {
+mongoose.connect(`mongodb://db:27017/books`,
+   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  },
+   },
   (error) => {
     if (error) console.log(error);
-  else {
-     console.log("Connect to database");
-}  
+    console.log("Connect to database");	   
   }
 );
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", express.static(__dirname));
-
-app.get("/api", (req, res) => {
-  res.status(200).json({
-    message: "Connect",
-  });
-});
 app.use("/books", books);
 
 app.listen(port, () =>
