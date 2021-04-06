87fcf920c5026254a5a6776925ac11a4c2f60197
Hùng Nguyễn Quốc <imbayonline@gmail.com>
2021-04-06 17:14:51 +0700

    modify topic

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4040;
-const mongoose = require("mongoose");
 const os = require("os");
-const books = require("./api/books");
 require("dotenv").config();
-
-mongoose.connect(os.platform() === "win32"
-? `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0-4veva.gcp.mongodb.net/chatops?retryWrites=true&w=majority`
-: `mongodb://db:27017/books`,
-   {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-   },
-  (error) => {
-    if (error) console.log(error);
-    console.log("Connect to database");	   
-  }
-);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/books", books);
+app.get("/compute", (req, res) => {
+  res.status(200).json({ message: "Compute time" });
+});
+
+app.post("/compute", (req, res) => {
+  let a = req.body.first_number;
+  let b = req.body.second_number;
+
+  return res
+    .status(200)
+    .json({ success: true, result: parseInt(a) + parseInt(b) });
+});
 
 app.listen(port, () =>
   console.log(`Listening on ${os.platform()} server on port ${port}`)
